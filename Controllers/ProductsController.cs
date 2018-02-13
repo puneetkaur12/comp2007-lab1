8cb3a7cd69469122ae8c3ea14284f349e639bed1
puneetkaur12 <200354592@student.georgianc.on.ca>
2018-02-13 16:10:36 -0500

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details
+        public ActionResult Details(string Blackview)
+        {
+            // check for an empty blackview
+            if(Blackview == null)
+            {
+                //return View("Error");
+                return RedirectToAction("Index");
+            }
+
+
+            ViewBag.Blackview = Blackview;
+            return View();
+        }
+    }
+}
